c012f391776e2f5e939b763d91737bd72eb5dcbd
Andrew Vancil <andrew.vancil@cchmc.org>
2024-06-05 14:52:04 -0400

    remove docker commands

--- app.R
+++ app.R
@@ -2,14 +2,12 @@ library(tidyr)
 library(readr)
 library(dplyr)
 library(shiny)
-library(stringr)
-
 
 # Define UI for data upload app ----
 ui <- fluidPage(
-  
+
   # App title ----
-  titlePanel("JFS CAN File Prep and Commands"),
+  titlePanel("JFS CAN File Prep"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -27,7 +25,7 @@ ui <- fluidPage(
                            "text/comma-separated-values,text/plain",
                            ".csv")),
       
-      downloadButton('download', "Save prepped file")
+      downloadButton('download', "Download Report")
       
     ),
     
@@ -44,40 +42,7 @@ ui <- fluidPage(
           h3("Prepped Data"),
           tableOutput("prepped"), width = 4)
       ),
-      fluidRow(
-        verbatimTextOutput("newname")
-      ),
-      h3("Docker Commands"),
-      fluidRow(
-        splitLayout(
-          cellWidths = c("1.5%", "85%", "10%"),
-          p(strong("1. ")),
-          verbatimTextOutput("command1"),
-          uiOutput("clip1")
-        )
-      ),
-      
-      hr(style = "border-top: 1px solid #FFF;"),
-      
-      fluidRow(
-        splitLayout(
-          cellWidths = c("1.5%", "85%", "10%"),
-          p(strong("2. ")),
-          verbatimTextOutput("command2"),
-          uiOutput("clip2")
-        )
-      ),
-      
-      hr(style = "border-top: 1px solid #FFF;"),
-      
-      fluidRow(
-        splitLayout(
-          cellWidths = c("1.5%", "85%", "10%"),
-          p(strong("3. ")),
-          verbatimTextOutput("command3"),
-          uiOutput("clip3")
-        )
-      )
+      verbatimTextOutput("console")
     )
     
   )
@@ -101,6 +66,7 @@ server <- function(input, output, session) {
     validate(
       need("CHILD_ADDRESS" %in% names(df),
            "No CHILD_ADRESS column found in the input file")
+      
     )
     
     
@@ -128,99 +94,45 @@ server <- function(input, output, session) {
     
   })
   
-  output$newname <- renderText({
-    
-    req(input$file)
-    
-    name <- str_sub(input$file$name, end = -5)
-    name_prepped <- paste0(name,"_prepped")
-    
-    glue::glue("New input file name: {name_prepped}.csv")
-  })
-  
-  
-  output$command1 <- renderPrint({
-    
-    req(input$file)
-    
-    name <- str_sub(input$file$name, end = -5)
-    name_prepped <- paste0(name,"_prepped")
-    
-    cmd1 <- glue::glue("docker run --rm -v ${{pwd}}:/tmp ghcr.io/degauss-org/geocoder {name_prepped}.csv")
-    
-    print(cmd1)
-    
-    output$clip1 <- renderUI({
-      req(input$file)
-      
-      rclipboard::rclipButton(
-        inputId = "clip1",
-        label = "Copy",
-        clipText = cmd1,
-        icon = icon("clipboard")
-      )
-    })
-
-  })
-  
-  
-  
-  output$command2 <- renderPrint({
-    
-    req(input$file)
-    
-    name <- str_sub(input$file$name, end = -5)
-    name_prepped <- paste0(name,"_prepped")
-    
-    cmd2 <- glue::glue("docker run --rm -v ${{pwd}}:/tmp ghcr.io/degauss-org/census_block_group {name_prepped}_geocoder_3.3.0_score_threshold_0.5.csv")
-    
-    print(cmd2)
-    
-    output$clip2 <- renderUI({
-      req(input$file)
-      
-      rclipboard::rclipButton(
-        inputId = "clip2",
-        label = "Copy",
-        clipText = cmd2,
-        icon = icon("clipboard")
-      )
-    })
-  })
-  
-  
-  output$command3 <- renderPrint({
-    
-    req(input$file)
-    
-    name <- str_sub(input$file$name, end = -5)
-    name_prepped <- paste0(name,"_prepped")
-    
-    cmd3 <- glue::glue("docker run --rm -v ${{pwd}}:/tmp degauss/jfs_aggregated_data_report:5.0.0 {name_prepped}_geocoder_3.3.0_score_threshold_0.5_census_block_group_0.6.0_2010.csv")
-    
-    print(cmd3)
-    
-    output$clip3 <- renderUI({
-      req(input$file)
-      
-      rclipboard::rclipButton(
-        inputId = "clip3",
-        label = "Copy",
-        clipText = cmd3,
-        icon = icon("clipboard")
-      )
-    })
-    
-  })
-  
   output$download <- downloadHandler(
-    filename = function() {
-      paste(str_sub(input$file$name, end = -5), "_prepped", ".csv", sep = "")
-    },
+    file <-  "intake_report.html",
     content = function(file) {
-      write.csv(d_prepped(), file, row.names = FALSE)
+      tempReport <- file.path(tempdir(), "intake_report.Rmd")
+      file.copy("intake_report.Rmd", tempReport, overwrite = TRUE)
+      params <- list(d_prepped = d_prepped())
+      rmarkdown::render(tempReport, 
+                        output_file = file,
+                        params = params,
+                        envir = new.env(parent = globalenv())
+      )
     }
   )
+  
+  # output$download <-  downloadHandler(
+  #       file <-  "intake_report.html",
+  #       content = function(file) {
+  #         tempReport <- file.path(tempdir(), "intake_report.Rmd")
+  #         file.copy("intake_report.Rmd", tempReport, overwrite = TRUE)
+  #         params <- list(d_prepped = d_prepped())
+  #         
+  #         withCallingHandlers({
+  #           shinyjs::html("console", "")
+  #           
+  #           rmarkdown::render(tempReport, 
+  #                             output_file = file,
+  #                             params = params,
+  #                             envir = new.env(parent = globalenv()),
+  #                             quiet = FALSE
+  #           )
+  #           
+  #         },
+  #         message = function(m) {
+  #           shinyjs::html(id = "console", html = m$message, add = TRUE)
+  #         })
+  #       }
+  # )
+  
+  
 }
 # Run the app ----
 shinyApp(ui, server)
