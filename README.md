0f6c981daea30a4efe85b6a8e6a37fe50e4571a5
Andrew Vancil <72818699+andrew-vancil@users.noreply.github.com>
2025-01-08 11:27:42 -0500

    2.0 (#8)
    * remove docker commands
    
    * no longer using docker
    
    * ignore test file
    
    * name repair work
    
    * download prepped csv file
    
    * add geocoding with addr
    
    * use pins to save finished dataset
    
    * rework selecting address to geocode
    
    * add spinners and pin message
    
    * tweak modal
    
    * add gut check tables
    
    * change output to neighborhood report; freshen up UI
    
    * use shinybusy for loading screen; remove low count censor
    
    * Update README.md for shiny app

--- README.md
+++ README.md
@@ -1,48 +1,15 @@
-# jfs_data <a href='https://degauss-org.github.io/DeGAUSS/'><img src='DeGAUSS_hex.png' align="right" height="138.5" /></a>
+# jfs_data 
 
-> DeGAUSS pipeline that generates one CSV file of aggregated, monthly data for JFS from a geocoded CSV file
+> Web application that receives a sacwis .csv input and saves an aggregated dataset
 
-[![Docker Build Status](https://img.shields.io/docker/automated/degauss/jfs_data)](https://hub.docker.com/repository/docker/degauss/jfs_data/tags)
-[![GitHub release (latest by date)](https://img.shields.io/github/v/release/degauss-org/jfs_data)](https://github.com/degauss-org/jfs_data/releases)
-
-## Instructions for Use
-
-### Open the JFS_data App
-
-1.  In Docker Desktop, in the top blue search bar, search for "degauss/jfs_data" and "Pull" the container
-
-2.  After pulling, navigate to "Images", find degauss/jfs_data and click "Run". In the popup box, expand the options and in the space marked "Local Host", type in "3838" and click Run
-
-3.  Navigate to the Containers page and select degauss/jfs_data. Towards top of the screen, click on the blue link that says "3838:3838" to launch the app.
-
-4.  Now that you're in the app, you can upload your .csv file
-
-5.  After the upload, the app will show you a sample of your original file and the prepared file for use in the next program
-
-6.  Click the button to download the prepped file. Save the downloaded file to a known location that you can navigate to (such as your downloads folder)
-
-7.  Now in PowerShell, navigate to your Downloads folder (it may be as simple as typing "cd Downloads")
-
-8.  Copy & paste the first Docker command into PowerShell and hit enter
-
-9.  Repeat for the 2nd and 3rd Docker commands, all in the Downloads folder
-
-10. After the 3rd command, you should have the final output, "monthly_report_v5.0.1.csv" that you can send along to Cole & Andrew
+1. Navigate to [salt.cchmc.org/jfs_data](https://salt.cchmc.org/jfs_data)
+2. In the top left, select "import file"
+3. Navigate to your sacwis output file and select it
+4. The app will now select the correct addresss to geocode, geocode the address and join it to a census tract
+     + This will take a few minutes
+5. Once done, check over the output to make sure it is accurate
+    + In the middle of the page, there are a couple suggested checks to complete
+6. If everything looks ok, back up in the top-left portion of the page, click "Save data"
+7. Once you've received the confirmation popup, feel free to close the web page
 
 
-## Dataset Notes
-
-- Screening status `SCREENED IN AR` included with `SCREENED IN`
-- Intakes that either have no listed address or were unsuccessfully geocoded will be included as a "neighborhood" called `Missing`
-
-## geomarker data
-
-- census tract-level [deprivation index](https://geomarker.io/dep_index/) from 2015 ACS measures
-- census tract-level population under age 18 from 2018 ACS
-- `00_create_tract_to_neighborhood.R` (based on code from Stu) makes `tract_to_neighborhood.rds` which is used to convert tracts to neighborhoods
-- `00_create_tract_to_neighborhood.R` also aggregates tract-level deprivation index (mean) and tract-level population under 18 (sum) to neighborhood and creates the neighborhood shapefile called `ham_neighborhoods_dep_index_shp.rds`
-- Neighborhood level fields with values less than 5 have been censored for privacy considerations
-
-## DeGAUSS details
-
-For detailed documentation on DeGAUSS, including general usage and installation, please see the [DeGAUSS README](https://degauss.org/).
--- app.R
+++ app.R
@@ -2,14 +2,13 @@ library(tidyr)
 library(readr)
 library(dplyr)
 library(shiny)
-library(stringr)
-
+library(addr)
 
 # Define UI for data upload app ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("JFS CAN File Prep and Commands"),
+  titlePanel("AFT File Ingestion"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -27,60 +26,38 @@ ui <- fluidPage(
                            "text/comma-separated-values,text/plain",
                            ".csv")),
       
-      downloadButton('download', "Save prepped file")
+      uiOutput('pin_button')
       
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
       
-      rclipboard::rclipboardSetup(),
-      
       fluidRow(
         column(
           h3("Original Data"),
           tableOutput("original"), width = 6),
         column(
-          h3("Prepped Data"),
+          h3("Geocoded Data"),
           tableOutput("prepped"), width = 4)
       ),
+      hr(),
       fluidRow(
-        verbatimTextOutput("newname")
+        h3('Summary Tables'),
+        column(tableOutput("summary_table_1"), width = 5),
+        column(tableOutput("summary_table_2"), width = 5)
+        ),
+      fluidRow(
+        p(strong("Checks:"),"num_addr_attempted_match + no_address_recorded = num_unique_person_dates"),
+        p("sum of `addr_match_result` table results should equal num_addr_attempted_match")
       ),
-      h3("Docker Commands"),
+      hr(),
       fluidRow(
-        splitLayout(
-          cellWidths = c("1.5%", "85%", "10%"),
-          p(strong("1. ")),
-          verbatimTextOutput("command1"),
-          uiOutput("clip1")
-        )
-      ),
-      
-      hr(style = "border-top: 1px solid #FFF;"),
-      
-      fluidRow(
-        splitLayout(
-          cellWidths = c("1.5%", "85%", "10%"),
-          p(strong("2. ")),
-          verbatimTextOutput("command2"),
-          uiOutput("clip2")
-        )
-      ),
-      
-      hr(style = "border-top: 1px solid #FFF;"),
-      
-      fluidRow(
-        splitLayout(
-          cellWidths = c("1.5%", "85%", "10%"),
-          p(strong("3. ")),
-          verbatimTextOutput("command3"),
-          uiOutput("clip3")
-        )
+        h3("Neighborhood Report"),
+        tableOutput("report_table")
+      )
       )
     )
-    
-  )
 )
 
 # Define server logic to read selected file ----
@@ -95,132 +72,265 @@ server <- function(input, output, session) {
     df <- read_csv(input$file$datapath)
     
     validate(
-      need("ALLEGATION_ADDRESS" %in% names(df),
-           "No ALLEGATION_ADRESS column found in the input file")
+      need("Allegation Address" %in% names(df),
+           "No Allegation Addresss column found in the input file")
     )
     validate(
-      need("CHILD_ADDRESS" %in% names(df),
-           "No CHILD_ADRESS column found in the input file")
+      need("Child's Address" %in% names(df),
+           "No Child's Address column found in the input file")
+      
     )
     
     
-    head(df[,c("INTAKE_ID", "ALLEGATION_ADDRESS", "CHILD_ADDRESS")])
+    head(df[,c("Intake ID", "Allegation Address", "Child's Address")])
     
-  })
+  }, digits = 0)
   
-  d_prepped <- reactive({
+  d_geocoded <- reactive({
     
     req(input$file)
-    
+
     d <- read_csv(input$file$datapath)
     
-    d <- d |> 
-      pivot_longer(cols = c(ALLEGATION_ADDRESS, CHILD_ADDRESS),
-                   names_to = 'address_type',
-                   values_to = 'address')
+    shinybusy::show_modal_progress_circle()
     
-    d_prepped <- d
+    d <- d |> 
+      rename_with(toupper, everything()) |> 
+      rename_with(~stringr::str_replace_all(.x, " ", "_"), everything()) |> 
+      rename(CHILD_ADDRESS = "CHILD'S_ADDRESS",
+             CHILD_ADDRESS_START = "CHILD'S_ADDRESS_START",
+             FAM_ASSES_DECISION = "FAM_ASSESS._DECISION",
+             MANDATED_REPORTER = "MANDATED_REPORTER?") 
+    
+    #just keep one record for each person-decision point; don't need record of initial decision and final decision
+    d <- d |> 
+      group_by(DECISION_DATE, PERSON_ID) |> 
+      slice_tail() |> 
+      ungroup()
+    
+    num_unique_person_dates <- nrow(d)
+    
+    #if two different addresses, keep allegation; if one address, keep whichever it is
+    d1 <- d |> 
+      filter(ALLEGATION_ADDRESS == "Unknown Address" | is.na(ALLEGATION_ADDRESS)) |> 
+      mutate(address = CHILD_ADDRESS)
+    
+    used_child_address <- nrow(d1)
+
+    d2 <- d |>
+      filter(!INTAKE_ID %in% d1$INTAKE_ID) |> 
+      mutate(address = ALLEGATION_ADDRESS)
+    
+    
+     d3 <- rbind(d1, d2) |> 
+       select(-c(ALLEGATION_ADDRESS, CHILD_ADDRESS))
+     
+     no_address_recorded <- nrow(
+       d3 |> 
+         filter(is.na(address)) 
+     )
+    
+    t <- d3 |> 
+      tidyr::drop_na(address) |> 
+      mutate(clean_address = toupper(clean_address_text(address))) 
+    
+    shinybusy::update_modal_progress(.2)
+    
+    t$addr <- as_addr(t$clean_address)
+    
+    shinybusy::update_modal_progress(.4)
+    
+    num_addr_attempted_match <- nrow(t)
+    
+    t$cagis_addr_matches <- addr_match(t$addr, cagis_addr$cagis_addr)
+    
+    shinybusy::update_modal_progress(.8)
+    
+    t2 <- t |>
+      mutate(
+        addr_match_result =
+          case_when(
+            purrr::map_lgl(cagis_addr_matches, is.null) ~ NA,
+            purrr::map_dbl(cagis_addr_matches, vctrs::vec_size) == 0 ~ "no_match",
+            purrr::map_dbl(cagis_addr_matches, vctrs::vec_size) == 1 ~ "single_match",
+            purrr::map_dbl(cagis_addr_matches, vctrs::vec_size) > 1 ~ "multi_match",
+            .default = "foofy"
+          ) |>
+          factor(levels = c("no_match", "single_match", "multi_match"))
+      )
+    
+    print_addr_matches <- t2 |> 
+      select(addr_match_result) |> 
+      group_by(addr_match_result) |> 
+      summarise(n_results = n()) |> 
+      ungroup()
+
+    
+    output$summary_table_1 <- renderTable({
+      req(input$file)
+      
+       tibble::enframe(c(num_unique_person_dates = num_unique_person_dates, 
+                         used_child_address = used_child_address, 
+                         no_address_recorded = no_address_recorded,
+                         num_addr_attempted_match = num_addr_attempted_match)) 
+    })
+    
+    output$summary_table_2 <- renderTable({  
+      print_addr_matches 
+    })
+    
+    t3 <- t2  |>
+      filter(addr_match_result %in% c("single_match")) |> 
+      #tibble::enframe(name = "input_addr", value = "cagis_addr") |>
+      dplyr::mutate(cagis_addr = purrr::list_c(cagis_addr_matches)) |>
+      dplyr::left_join(cagis_addr, by = "cagis_addr")
+    
+    shinybusy::update_modal_progress(.9)
+    
+    t4 <- t3 |> 
+      tidyr::unnest(cagis_addr_data) |> 
+      tidyr::drop_na(cagis_s2) |> 
+      mutate(block_group_id = tiger_block_groups(s2::as_s2_cell(cagis_s2)), year = '2010') 
+    
+    t5 <- t4|> 
+      mutate(census_tract_id = stringr::str_sub(block_group_id, end = -2)) 
+    
+    d_geocoded <- t5 |> 
+      select(INTAKE_ID, SCREENING_DECISION, DECISION_DATE, BIRTH_DATE, PERSON_ID, RACE,
+             MANDATED_REPORTER, block_group_id, census_tract_id) 
+    
+
+    shinybusy::update_modal_progress(1)
+
+    shinybusy::remove_modal_progress()
+    
+    d_geocoded
+
   })
   
   output$prepped <- renderTable({
     
-    head(d_prepped()[, c("INTAKE_ID",  "address")])
+    req(d_geocoded())
+    
+    head(d_geocoded()[, c("INTAKE_ID", "census_tract_id")])
+    
+  }, digits = 0)
+  
+  d_report <- reactive({
+    req(d_geocoded())
+    
+    tract_to_neighborhood <- readRDS('tract_to_neighborhood.rds')
+    
+    r <- d_geocoded()
+    
+    r <- r |>
+      mutate(screened_in = SCREENING_DECISION %in% c("SCREENED IN", "SCREENED IN AR"))
+    
+    ##
+    r <- r  |> 
+      mutate(DECISION_DATE = lubridate::as_date(DECISION_DATE, format = "%m/%d/%y"),
+             BIRTH_DATE = lubridate::as_date(BIRTH_DATE, format = "%m/%d/%y")) |> 
+      mutate(month = lubridate::month(DECISION_DATE),
+             year = lubridate::year(DECISION_DATE)) 
+    
+    #get age, sort into groups
+    r <- r |>
+      mutate(BIRTH_DATE = replace_na(BIRTH_DATE, as.Date('1900-01-01'))) |>
+      mutate(age = as.numeric(difftime(DECISION_DATE, BIRTH_DATE, units = "weeks")/52.25)) |>
+      mutate(age_grp = case_when(age <= 5.0 ~ "zero_five",
+                                 age < 19 ~ "five_plus",
+                                 age >= 19 ~ "no_birth_date"))
+    d_neigh <- r  |>
+      mutate(fips_tract_id = as.character(census_tract_id)) |> #comment these out when testing with concentrated data
+      left_join(tract_to_neighborhood, by='fips_tract_id') |>
+      filter(!is.na(DECISION_DATE))
+    
+    screen_neighborhood_age <- d_neigh |>
+      group_by(neighborhood, year, month, age_grp) |>
+      summarise(n_screened_in = sum(screened_in == TRUE, na.rm = TRUE),
+                n_calls = n(),
+                .groups = "keep"
+      ) |>
+      ungroup() 
+    
+    screen_neighborhood_MR <- d_neigh |>
+      group_by(neighborhood, year, month, MANDATED_REPORTER) |>
+      summarise(n_screened_in = sum(screened_in == TRUE, na.rm = TRUE),
+                n_calls = n(),
+                .groups = "keep"
+      ) |>
+      ungroup() 
+    
+    screen_neighborhood_age_report <- screen_neighborhood_age |> 
+      group_by(year, month, age_grp) |>
+      summarise(neighborhood = "Total",
+                n_calls = sum(n_calls),
+                n_screened_in = sum(n_screened_in)) %>%
+      bind_rows(screen_neighborhood_age, .) |>
+      rename(group = "age_grp")
+    
+    screen_neighborhood_MR_report <- screen_neighborhood_MR |> 
+      group_by(year, month, MANDATED_REPORTER) |>
+      summarise(neighborhood = "Total",
+                n_calls = sum(n_calls),
+                n_screened_in = sum(n_screened_in)) %>%
+      bind_rows(screen_neighborhood_MR, .)
+    
+    screen_neighborhood_MR_report <- screen_neighborhood_MR_report |>
+      mutate(MANDATED_REPORTER = recode_factor(MANDATED_REPORTER, Yes = "MR", No = "Non_MR")) |>
+      rename(group = "MANDATED_REPORTER")
+    
+    
+    screen_neighborhood <- rbind(screen_neighborhood_age_report, screen_neighborhood_MR_report) |>
+      arrange(neighborhood, year, month)
+    
+    screen_neighborhood_2 <- screen_neighborhood |>
+      group_by(neighborhood, year, month) |>
+      summarise(group = "Total",
+                n_calls = sum(n_calls),
+                n_screened_in = sum(n_screened_in)) %>%
+      bind_rows(screen_neighborhood, .) |>
+      arrange(neighborhood, year, month)
+    
+    screen_neighborhood_rate <- screen_neighborhood_2 |>
+      rowwise() |>
+      mutate(screen_in_rate = round(n_screened_in / n_calls, 2)) 
+    
+    d_report <- screen_neighborhood_rate |> 
+      drop_na(neighborhood)
     
   })
   
-  output$newname <- renderText({
+  output$report_table <- renderTable({
+    head(d_report() |> filter(neighborhood == "Avondale") |> select(-screen_in_rate))
+  }, digits = 0)
+  
+  output$pin_button <- renderUI({
+    req(d_report())
     
-    req(input$file)
+    actionButton('pin', "Save data")
+  })
+  
+  observeEvent(input$pin, {
     
-    name <- str_sub(input$file$name, end = -5)
-    name_prepped <- paste0(name,"_prepped")
+    salt_board <- pins::board_connect(auth = "manual",
+                                      server = Sys.getenv("CONNECT_SERVER_DEV"),
+                                      key = Sys.getenv("CONNECT_API_KEY_DEV"))
     
-    glue::glue("New input file name: {name_prepped}.csv")
+    salt_board |> 
+      pins::pin_write(d_report(), "AFT_intake_neigbhorhood_report")
+    
+    showModal(
+      modalDialog(
+        title = "Data successfully saved!",
+        "You may now exit this application",
+        easyClose = TRUE,
+        footer = NULL
+    ))
+    
   })
   
   
-  output$command1 <- renderPrint({
-    
-    req(input$file)
-    
-    name <- str_sub(input$file$name, end = -5)
-    name_prepped <- paste0(name,"_prepped")
-    
-    cmd1 <- glue::glue("docker run --rm -v ${{pwd}}:/tmp ghcr.io/degauss-org/geocoder {name_prepped}.csv")
-    
-    print(cmd1)
-    
-    output$clip1 <- renderUI({
-      req(input$file)
-      
-      rclipboard::rclipButton(
-        inputId = "clip1",
-        label = "Copy",
-        clipText = cmd1,
-        icon = icon("clipboard")
-      )
-    })
-
-  })
-  
-  
-  
-  output$command2 <- renderPrint({
-    
-    req(input$file)
-    
-    name <- str_sub(input$file$name, end = -5)
-    name_prepped <- paste0(name,"_prepped")
-    
-    cmd2 <- glue::glue("docker run --rm -v ${{pwd}}:/tmp ghcr.io/degauss-org/census_block_group {name_prepped}_geocoder_3.3.0_score_threshold_0.5.csv")
-    
-    print(cmd2)
-    
-    output$clip2 <- renderUI({
-      req(input$file)
-      
-      rclipboard::rclipButton(
-        inputId = "clip2",
-        label = "Copy",
-        clipText = cmd2,
-        icon = icon("clipboard")
-      )
-    })
-  })
-  
-  
-  output$command3 <- renderPrint({
-    
-    req(input$file)
-    
-    name <- str_sub(input$file$name, end = -5)
-    name_prepped <- paste0(name,"_prepped")
-    
-    cmd3 <- glue::glue("docker run --rm -v ${{pwd}}:/tmp degauss/jfs_aggregated_data_report:5.0.0 {name_prepped}_geocoder_3.3.0_score_threshold_0.5_census_block_group_0.6.0_2010.csv")
-    
-    print(cmd3)
-    
-    output$clip3 <- renderUI({
-      req(input$file)
-      
-      rclipboard::rclipButton(
-        inputId = "clip3",
-        label = "Copy",
-        clipText = cmd3,
-        icon = icon("clipboard")
-      )
-    })
-    
-  })
-  
-  output$download <- downloadHandler(
-    filename = function() {
-      paste(str_sub(input$file$name, end = -5), "_prepped", ".csv", sep = "")
-    },
-    content = function(file) {
-      write.csv(d_prepped(), file, row.names = FALSE)
-    }
-  )
 }
 # Run the app ----
 shinyApp(ui, server)
